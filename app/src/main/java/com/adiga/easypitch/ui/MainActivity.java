ee520a9ffa7913853c8793d85be3048bfbf888e9
dbajj <daniel.bajj@gmail.com>
2017-11-29 21:55:56 -0800

    - Add fragment to handle audio processing in main activity

--- app/src/main/java/com/adiga/easypitch/ui/MainActivity.java
+++ app/src/main/java/com/adiga/easypitch/ui/MainActivity.java
@@ -1,13 +1,24 @@
 package com.adiga.easypitch.ui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioRecord;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.adiga.easypitch.R;
 import com.adiga.easypitch.io.MicrophoneIO;
+import com.adiga.easypitch.pitch.PitchDetector;
 
+import org.apache.commons.collections.Buffer;
 import org.w3c.dom.Text;
 
 import java.util.concurrent.LinkedBlockingQueue;
@@ -21,7 +32,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MicrophoneIO audioInput = new MicrophoneIO();
+        getPermissions();
+
+        addAudioFragment();
+
+        //TODO add this functionality into the AudioFragment
+        /*MicrophoneIO audioInput = new MicrophoneIO();
 
         audioOutputTextID = (TextView) findViewById(R.id.audio_sample);
 
@@ -31,21 +47,61 @@ public class MainActivity extends AppCompatActivity {
 
         audioInput.startRecording();
 
+
         //TODO figure out why this queue is only ever outputting 0.0;
-        LinkedBlockingQueue<Double> audioInputQueue = audioInput.getAudioInput();
+
+        Buffer audioInputQueue = audioInput.getAudioInput();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                try {
-                 audioOutputTextID.setText(audioInputQueue.take().toString());
+                double[] waveInput = new double[audioInputQueue.size()];
+
+
+                if(audioInputQueue.size() >= 4096) {
+
+                    for(int i = 0; i < audioInputQueue.size(); i ++) {
+                        waveInput[i] = (double) audioInputQueue.get();
+                    }
+                    String foundPitch = String.valueOf(PitchDetector.findPitch(waveInput,44100));
+                    audioOutputTextID.setText(foundPitch);
+
+                }
+
+
                 handler.postDelayed(this,delay);
 
-                } catch(InterruptedException e) {
-                    e.printStackTrace();
-                }
             }
-        },delay);
+        },delay); */
 
+
+    }
+
+    @Override
+    public void onResume() {
+       super.onResume();
+
+        AudioFragment myAudio = (AudioFragment) getSupportFragmentManager().findFragmentByTag("audio_processor");
+
+    }
+
+    private void getPermissions() {
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},5);
+        }
+    }
+
+    private void addAudioFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        AudioFragment audioFragment = new AudioFragment();
+
+        fragmentTransaction.add(audioFragment,"audio_processor");
+
+        fragmentTransaction.commit();
 
     }
 }
